e04aec5b9eaf2e1df09daf1e4f49c296c26d47bb
Max Toro <maxtoroq@gmail.com>
2018-01-03 19:59:07 -0300

    auto-download trang

--- schemas/convert-to-xsd.ps1
+++ schemas/convert-to-xsd.ps1
@@ -1,11 +1,25 @@
 ﻿$ErrorActionPreference = "Stop"
 Push-Location (Split-Path $script:MyInvocation.MyCommand.Path)
 
+function script:EnsureTrang {
+
+   if (-not (Test-Path $trangPath -PathType Container)) {
+      Add-Type -AssemblyName System.IO.Compression.FileSystem
+      $trangTemp = Join-Path (Resolve-Path .) trang.zip
+      Invoke-WebRequest https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/jing-trang/trang-20091111.zip -OutFile $trangTemp
+      [IO.Compression.ZipFile]::ExtractToDirectory($trangTemp, (Resolve-Path .))
+      rm $trangTemp
+   }
+}
+
 try {
 
    $saxonPath = (Resolve-Path ..\packages\Saxon-HE.*)[0]
+   $trangPath = Join-Path (Resolve-Path .) trang-20091111
 
-   java -jar \lib\trang\20091111\trang.jar -o any-process-contents=lax -o indent=3 xcst-app.rng xcst-app.xsd
+   EnsureTrang
+
+   java -jar $trangPath\trang.jar -o any-process-contents=lax -o indent=3 xcst-app.rng xcst-app.xsd
 
    &"$saxonPath\tools\Transform" -s:xcst-app.xsd -xsl:xsd-comment.xsl -o:xcst-app.xsd
 
