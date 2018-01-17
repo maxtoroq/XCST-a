26b084848ced70ba353282a2148d5905a6681845
Max Toro <maxtoroq@gmail.com>
2018-01-16 21:05:31 -0300

    copying roslyn binaries on restore script

--- build/restore-packages.ps1
+++ build/restore-packages.ps1
@@ -9,7 +9,12 @@ try {
    ./ensure-nuget.ps1
 
    &$nuget restore $solutionPath\XCST-a.sln
-   &$nuget restore ..\samples\aspnet\packages.config -SolutionDirectory ..
+
+   foreach ($web in ls ..\samples\* -Directory) {
+      &$nuget restore $web\packages.config -SolutionDirectory ..
+      Copy-Item ..\packages\Microsoft.Net.Compilers.*\tools\*.dll $web\Bin\roslyn
+      Copy-Item ..\packages\Microsoft.Net.Compilers.*\tools\*.exe $web\Bin\roslyn
+   }
 
 } finally {
    Pop-Location
