49fb5b2e9fc2f1b807ff5768499be32136d77643
Max Toro <maxtoroq@gmail.com>
2019-10-01 20:33:15 -0300

    xcst upgrade

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -13,7 +13,7 @@ namespace Xcst {
 
    partial class AssemblyInfo {
 
-      public const string XcstMajorMinor = "0.62";
+      public const string XcstMajorMinor = "0.63";
       public const string XcstAssemblyVersion = "1.0.0";
       public const string XcstAssemblyFileVersion = XcstMajorMinor + "." + XcstPatch;
       public const string XcstAssemblyInformationalVersion = XcstAssemblyFileVersion;
