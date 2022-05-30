f1e0651ad12b7646543dd0483bbe07cac5bb04d7
Max Toro <maxtoroq@gmail.com>
2022-05-30 15:20:30 -0400

    removed saxon dep on convert-to-xsd

--- schemas/convert-to-xsd.ps1
+++ schemas/convert-to-xsd.ps1
@@ -7,21 +7,28 @@ function EnsureTrang {
       $trangTemp = Join-Path (Resolve-Path .) trang.zip
       [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
       Invoke-WebRequest https://github.com/relaxng/jing-trang/releases/download/V20181222/trang-20181222.zip -OutFile $trangTemp
-      Expand-Archive $trangTemp (Resolve-Path .)
+      Expand-Archive $trangTemp $trangPath
       rm $trangTemp
    }
 }
 
 try {
 
-   $saxonPath = Resolve-Path ..\packages\Saxon-HE.*
-   $trangPath = Join-Path (Resolve-Path .) trang-20181222
+   Add-Type -AssemblyName System.Xml.Linq
+
+   $trangPath = Join-Path (Resolve-Path .) trang-
 
    EnsureTrang
 
-   java -jar $trangPath\trang.jar -o any-process-contents=lax -o indent=3 xcst-app.rng xcst-app.xsd
+   $trangJar = Resolve-Path $trangPath\*\trang.jar
+   $xsdName = "xcst-app.xsd"
 
-   &"$saxonPath\tools\Transform" -s:xcst-app.xsd -xsl:xsd-comment.xsl -o:xcst-app.xsd
+   java -jar $trangJar -o any-process-contents=lax -o indent=3 xcst-app.rng $xsdName
+
+   $xsd = [Xml.Linq.XDocument]::Load((Resolve-Path $xsdName), 'PreserveWhitespace')
+   [Xml.Linq.XComment]$comment = $xsd.Nodes() | where { $_ -is [Xml.Linq.XComment] } | select -First 1
+   $comment.Value = " Converted from Relax NG schema, using Trang. Use only with code completion tools that do not support Relax NG. "
+   $xsd.Save((Resolve-Path $xsdName))
 
 } finally {
    Pop-Location
